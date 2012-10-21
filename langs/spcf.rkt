7578ad8b3e9500e657deb7dd694de3f07cf14a54
David Van Horn <dvanhorn@ccs.neu.edu>
2012-10-21 19:40:42 -0400

    More re-use in SPCF.

--- langs/spcf.rkt
+++ langs/spcf.rkt
@@ -2,5 +2,5 @@
 (require (for-syntax racket/base "make-lang.rkt")
          "../spcf.rkt")
 (provide #%top-interaction #%module-begin)
-(define-syntax #%top-interaction (make-#%top-interaction #'-->s))
-(define-syntax #%module-begin    (make-#%module-begin    #'-->s))
+(define-syntax #%top-interaction (make-#%top-interaction #'-->sv))
+(define-syntax #%module-begin    (make-#%module-begin    #'-->sv))
--- spcf.rkt
+++ spcf.rkt
@@ -1,6 +1,6 @@
 #lang racket
-(provide SPCF s -->s)
-(require redex/reduction-semantics "subst.rkt" "pcf.rkt")
+(provide SPCF s sv -->sv)
+(require redex/reduction-semantics "pcf.rkt")
 
 (define-extended-language SPCF PCF
   ;; Values
@@ -9,25 +9,22 @@
 (define s
   (reduction-relation
    SPCF
-   (--> ((λ ([X : T] ..._1) M) V ..._1)
-        (subst (X V) ... M)
-        β)
    (--> ((• (T ..._1 -> T_0)) V ..._1)
         (• T_0)
         β•)
-   (--> ((• (T_0 ..._1 T T_1 ... -> T_o)) V_0 ..._1 V V_1 ...)
+   (--> ((• (T_0 ..._1 T T_1 ... -> T_o)) 
+             V_0 ..._1 V V_1 ...)
         (havoc T V)
         havoc)
    (--> (O V_0 ...) M
         (judgment-holds (δ^ O (V_0 ...) M))
-        δ)
-   (--> (if0 0 M_0 M_1) M_0 if0-t)
-   (--> (if0 N M_0 M_1) M_1
-        (side-condition (not (zero? (term N))))
-        if0-f)
+        δ^)
    (--> (if0 (• nat) M_0 M_1) M_0 if•-t)
    (--> (if0 (• nat) M_0 M_1) M_1 if•-f)))
    
+(define sv
+  (union-reduction-relations s (extend-reduction-relation v SPCF)))
+
 (define-metafunction SPCF
   [(havoc nat M) M]
   [(havoc (T_0 ... -> T_1) M)
@@ -41,7 +38,7 @@
   [(not-div? div) #f]
   [(not-div? any) #t])
 
-(define -->s (context-closure s SPCF E))
+(define -->sv (context-closure sv SPCF E))
 
 (define-judgment-form SPCF
   #:mode (δ^ I I O)
@@ -51,6 +48,4 @@
   [(δ^ quotient ((• nat) N)   (• nat))
    (side-condition (not-zero? N))]
   [(δ^ O (any_0 ... (• nat) any_1 ...) (• nat))
-   (side-condition (not-div? O))]
-  [(δ^ O (N_0 ...) M)
-   (where M (δf O (N_0 ...)))])
+   (side-condition (not-div? O))])
