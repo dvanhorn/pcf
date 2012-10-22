c4a402f63d892879f7766d68706072b0db7d9bc8
David Van Horn <dvanhorn@ccs.neu.edu>
2012-10-22 11:53:40 -0400

    Better arrow support.

--- langs/lexical.rkt
+++ langs/lexical.rkt
@@ -4,20 +4,27 @@
   (syntax-case stx ()
     [(_ e) #'(void (λ () (lx e)))]))
 
+(require (for-syntax syntax/stx))
 (define-syntax (lx stx)
   (syntax-case stx (λ • : err ⚖)
-    [(_ (λ ([x : _] ...) e))
-     #'(λ (x ...) (lx e))]
-    [(_ (if0 e0 e1 e2))
-     #'(if (lx e0)
-           (lx e1)
-           (lx e2))]
-    [(_ (err _ string)) #'string]
-    [(_ (• t)) #''(• t)]
-    [(_ (c ⚖ e)) #'(begin (lc c) (lx e))] ; FIXME
-    [(_ (e ...))
-     #'((lx e) ...)]
-    [(_ e) #'e]))
+     [(_ (λ ([x : _] ...) e))
+      (nice stx #'(λ (x ...) (lx e)))]
+     [(_ (if0 e0 e1 e2))
+      (nice stx #'(if (lx e0)
+                      (lx e1)
+                      (lx e2)))]
+     [(_ (err _ string)) #'string]
+     [(_ (• t)) #''(• t)]
+     [(_ (c ⚖ e)) #'(begin (lc c) (lx e))]
+     [(_ (e ...))
+      #'((lx e) ...)]
+     [(_ e) #'e]))
+
+(define-for-syntax (nice ctx stx)
+  (syntax-track-origin  
+   stx
+   (stx-car (stx-cdr ctx))
+   (syntax-local-introduce (stx-car (stx-car (stx-cdr ctx))))))
 
 (define-syntax (lc stx)
   (syntax-case stx (->)
