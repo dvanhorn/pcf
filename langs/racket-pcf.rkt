23c50dc22b2a91f7608885473ddaa278ccbd6b83
David Van Horn <dvanhorn@ccs.neu.edu>
2012-11-27 01:41:56 -0500

    Provide pos?, any?, prime?.

--- langs/racket-pcf.rkt
+++ langs/racket-pcf.rkt
@@ -1,2 +1,6 @@
 #lang racket
-(provide #%app #%datum add1 sub1 * + quotient λ -> (rename-out [if if0]))
+(provide #%app #%datum add1 sub1 * + quotient λ -> 
+         (rename-out [if if0]
+                     [positive? pos?]
+                     [identity any?]
+                     [identity prime?]))
