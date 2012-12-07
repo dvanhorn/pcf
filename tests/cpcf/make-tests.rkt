c19d2d2b9e2508b0a37f32fb25168dd2f3811684
David Van Horn <dvanhorn@ccs.neu.edu>
2012-12-07 16:45:07 -0500

    Some tests for CPCF.

---
+++ tests/cpcf/make-tests.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(provide make-cpcf-tests)
+(require redex/reduction-semantics cpcf/redex
+         (rename-in redex/reduction-semantics [term quote]))
+
+(define (make-cpcf-tests -->cv typable/contract?)
+  (test-->> -->cv '(pos? + - ⚖ 1) '1)
+  (test-->> -->cv '(pos? + - ⚖ 0) '(blame +))
+
+  (test-equal (typable/contract? '(pos? ⚖ 1)) #t)
+  (test-equal (typable/contract? '(pos? ⚖ add1)) #f))
--- tests/cpcf/redex.rkt
+++ tests/cpcf/redex.rkt
@@ -1,3 +1,6 @@
 #lang racket
-(require tests/pcf/make-tests cpcf/redex)
+(require tests/pcf/make-tests
+         tests/cpcf/make-tests
+         cpcf/redex)
 (make-pcf-tests -->cv typable/contract?)
+(make-cpcf-tests -->cv typable/contract?)
