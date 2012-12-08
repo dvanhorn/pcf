6982d48d6231421a1b64c7bb947a04907fad5821
David Van Horn <dvanhorn@ccs.neu.edu>
2012-12-07 19:13:47 -0500

    Some tests for SCPCF.

---
+++ tests/scpcf/make-tests.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(provide make-scpcf-tests)
+(require redex/reduction-semantics scpcf/redex
+         (rename-in redex/reduction-semantics [term quote]))
+
+(define (make-scpcf-tests -->scv typable/contract/symbolic?)
+  (test-->> -->scv '(pos? + - ⚖ (• nat)) '(• nat pos?) '(blame +))
+  (test-->> -->scv '((• (nat -> nat)) + - ⚖ 7) '7 '(blame +))
+
+  (test-equal (typable/contract/symbolic? '(pos? ⚖ (• nat))) #t)
+  (test-equal (typable/contract/symbolic? '(pos? ⚖ (• (nat -> nat)))) #f)
+  ;; This tests cause errors because of
+  ;; http://lists.racket-lang.org/users/archive/2012-December/055486.html
+  #;(test-equal (typable/contract/symbolic? '((• (nat -> nat)) ⚖ 7)) #t)
+  #;(test-equal (typable/contract/symbolic? '((• (nat -> nat)) ⚖ (• nat))) #t))
--- tests/scpcf/redex.rkt
+++ tests/scpcf/redex.rkt
@@ -1,4 +1,8 @@
 #lang racket
-(require tests/pcf/make-tests tests/spcf/make-tests scpcf/redex)
+(require tests/pcf/make-tests
+         tests/spcf/make-tests
+         tests/scpcf/make-tests
+         scpcf/redex)
 (make-pcf-tests -->scv typable/contract/symbolic?)
 (make-spcf-tests -->scv typable/contract/symbolic?)
+(make-scpcf-tests -->scv typable/contract/symbolic?)
