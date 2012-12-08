829df048e3c34e5c3d64d8a52da6a58608c0d83a
David Van Horn <dvanhorn@ccs.neu.edu>
2012-12-08 12:46:15 -0500

    Test case for abort with error.
    This test case creates an evaluation context in CPCF and an error that
    should abort it, but because the PCF err-abort rule uses a
    metafunction for checking that E is a non-empty *PCF* context, it
    should cause a domain error.  It *should* have caused an error, but by
    luck, the monitor form is considered an application in PCF.  Thus the
    test passes by luck (the worst thing possible).

--- tests/cpcf/make-tests.rkt
+++ tests/cpcf/make-tests.rkt
@@ -7,6 +7,9 @@
   (test-->> -->cv '(pos? + - ⚖ 1) '1)
   (test-->> -->cv '(pos? + - ⚖ 0) '(blame +))
   (test-->> -->cv '(add1 (pos? + - ⚖ 0)) '(blame +))
+  ;; This doesn't trigger a bug caused by extension because by sheer
+  ;; luck the monitor form looks like an applicaton in PCF.
+  (test-->> -->cv '((λ ([x : nat]) (pos? + - ⚖ 1)) (err nat "e")) '(err nat "e"))
 
   (test-equal (typable/contract? '(pos? ⚖ 1)) #t)
   (test-equal (typable/contract? '(pos? ⚖ add1)) #f))
