472f0caf8b3f57cadcb734cf3da0e68c057cd424
David Van Horn <dvanhorn@ccs.neu.edu>
2012-12-09 00:04:11 -0500

    Fix another keyword bug.

--- pcf/lang.rkt
+++ pcf/lang.rkt
@@ -4,6 +4,8 @@
          pcf/redex
 	 pcf/private/racket-pcf
          pcf/private/label)
-(provide #%top-interaction #%module-begin err (all-from-out pcf/private/racket-pcf))
+(provide #%top-interaction #%module-begin
+         (all-from-out pcf/private/label)
+         (all-from-out pcf/private/racket-pcf))
 (define-syntax #%top-interaction (make-#%top-interaction #'-->v typable?))
 (define-syntax #%module-begin    (make-#%module-begin    #'-->v typable?))
