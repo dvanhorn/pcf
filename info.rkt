3d704a11a22f68a562198cc58f28a1c6a814d36e
David Van Horn <dvanhorn@ccs.neu.edu>
2014-05-13 10:24:55 -0400

    Dependencies.

--- info.rkt
+++ info.rkt
@@ -2,4 +2,15 @@
 
 (define collection 'multi)
 
+(define deps 
+  '("base" 
+    "redex-lib" 
+    "redex-pict-lib" 
+    "unstable-lib"))
 
+(define build-deps 
+  '("scribble-lib" 
+    "racket-doc" 
+    "sandbox-lib" 
+    "redex-doc" 
+    "scribble-doc"))
