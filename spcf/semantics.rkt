358fb35490b105493479ec0905ee553ee2af99e3
David Van Horn <dvanhorn@ccs.neu.edu>
2014-06-24 11:24:30 -0400

    Add mu^ to SPCF.

--- spcf/semantics.rkt
+++ spcf/semantics.rkt
@@ -1,13 +1,15 @@
 #lang racket
-(provide s sv -->sv havoc δ^)
+(provide sv -->sv havoc δ^)
 (require redex/reduction-semantics
          spcf/syntax
+         pcf/private/subst
          pcf/semantics)
 
-(define s
-  (reduction-relation
+(define sv
+  (extend-reduction-relation v
    SPCF #:domain M
    (--> Ω Ω Ω)
+   (--> (μ (X : T) V) (subst (X (• T)) V) μ) ;; μ^
    (--> (@ L (• (T_0 ..._1 -> T)) V ..._1) (• T) β•)
    (--> (@ L (• (T_0 ..._1 T T_1 ... -> T_o))
 	     V_0 ..._1 V V_1 ...)
@@ -18,9 +20,6 @@
 	δ^)
    (--> (if0 (• nat) M_0 M_1) M_0 if•-t)
    (--> (if0 (• nat) M_0 M_1) M_1 if•-f)))
-
-(define sv
-  (union-reduction-relations s (extend-reduction-relation v SPCF)))
 
 (define-metafunction SPCF
   havoc : T M -> M
