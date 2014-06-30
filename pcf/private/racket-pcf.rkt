d27f0a1c7a0c48407b91ec2cf2a97065798cecb1
David Van Horn <dvanhorn@ccs.neu.edu>
2014-06-30 01:19:06 -0400

    Syntax bindings for new prims.

--- pcf/private/racket-pcf.rkt
+++ pcf/private/racket-pcf.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(provide #%app #%datum add1 sub1 * + quotient λ -> zero?
+(provide #%app #%datum add1 sub1 * + - < > <= => not = quotient λ -> zero?
          (rename-out [positive? pos?]
                      [identity any?]
                      [identity prime?]))
