04ac141fd9cd3b3415a2cfcd1d2439176ff5ef71
David Van Horn <dvanhorn@ccs.neu.edu>
2014-06-30 02:26:44 -0400

    Hash lang for CPCF with heap.

---
+++ cpcf/heap/lang.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require (for-syntax racket/base pcf/private/make-lang)
+         (for-syntax (only-in cpcf/redex typable/contract?))
+         cpcf/redex
+         pcf/private/racket-pcf
+         pcf/private/label
+         pcf/private/return)
+(provide #%top-interaction #%module-begin
+         (all-from-out pcf/private/label)
+         (all-from-out pcf/private/racket-pcf))
+(define-syntax #%top-interaction (make-#%top-interaction #'-->cv typable/contract? #'injσ #'returnσ))
+(define-syntax #%module-begin    (make-#%module-begin    #'-->cv typable/contract? #'values #'return #'pp #'color))
