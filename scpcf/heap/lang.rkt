674caeab9e20586c077e4339a2ba6dfaadea4233
David Van Horn <dvanhorn@ccs.neu.edu>
2014-06-30 07:36:25 -0400

    Hash lang for SCPCF with heap.

---
+++ scpcf/heap/lang.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require (for-syntax racket/base pcf/private/make-lang)
+         (for-syntax (only-in scpcf/redex typable/contract/symbolic?))
+         scpcf/heap/semantics
+         pcf/private/racket-pcf
+         pcf/private/label
+         pcf/private/return)
+(provide #%top-interaction #%module-begin
+         (all-from-out pcf/private/label)
+         (all-from-out pcf/private/racket-pcf))
+(define-syntax #%top-interaction (make-#%top-interaction #'-->scvσ typable/contract/symbolic? #'injσ #'returnσ))
+(define-syntax #%module-begin    (make-#%module-begin    #'-->scvσ typable/contract/symbolic? #'injσ #'returnσ #'ppσ #'color))
---
+++ scpcf/heap/lang/reader.rkt
@@ -0,0 +1,2 @@
+#lang s-exp syntax/module-reader
+scpcf/heap/lang
