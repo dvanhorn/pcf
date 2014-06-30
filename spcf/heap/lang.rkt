611b85622dde61b0a5af67009021dc5d0b483ddc
David Van Horn <dvanhorn@ccs.neu.edu>
2014-06-30 01:24:51 -0400

    Hash lang for SPCF with heap.

---
+++ spcf/heap/lang.rkt
@@ -0,0 +1,13 @@
+#lang racket
+(require (for-syntax racket/base pcf/private/make-lang)
+         (for-syntax (only-in pcf/types typable?))
+         spcf/heap/semantics
+         pcf/heap/semantics
+	 pcf/private/racket-pcf
+         pcf/private/label
+         pcf/private/return)
+(provide #%top-interaction #%module-begin
+         (all-from-out pcf/private/label)
+         (all-from-out pcf/private/racket-pcf))
+(define-syntax #%top-interaction (make-#%top-interaction #'-->svσ typable? #'injσ #'returnσ))
+(define-syntax #%module-begin    (make-#%module-begin    #'-->svσ typable? #'injσ #'returnσ #'pp #'color))
